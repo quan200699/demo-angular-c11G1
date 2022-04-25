bd493bb8a117401d91bd1dc81edbc2fa29cc12b5
quan200699 <maplestory2012vn@gmail.com>
2022-04-25 09:26:32 +0700

    add product

---
+++ src/app/product-create/product-create.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {Product} from '../model/product';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+  product: Product = {};
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  createProduct() {
+    console.log(this.product);
+  }
+}
